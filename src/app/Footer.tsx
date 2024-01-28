d255fae72482cad82018d35bcafb39423008af60
RaySlash <stevemathewjoy@gmail.com>
2024-01-28 20:24:08 +1000

    app: inital site logic

---
+++ src/app/Footer.tsx
@@ -0,0 +1,6 @@
+const Footer = () => {
+  const year = new Date().getFullYear();
+  return <footer>{`Copyright @ ${year}`}</footer>;
+};
+
+export default Footer;
---
+++ src/app/Navbar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+    <div>
+      <div className="navbar">
+        <ul>
+          <li>Home</li>
+          <li>Projects</li>
+          <li>Photography</li>
+          <li>Blog</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
