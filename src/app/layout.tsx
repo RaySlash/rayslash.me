853d703eab2d8dfde19b2716dc98256e4ff45350
RaySlash <stevemathewjoy@gmail.com>
2024-01-30 15:18:17 +1000

    vercel: add SpeedInsights

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,30 @@
 import type { Metadata } from "next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 import { Atkinson_Hyperlegible } from "next/font/google";
 import "./globals.css";
 
 export const metadata: Metadata = {
-  title: "Steve Mathew Joy",
-  description: "Personal Website",
+	title: "Steve Mathew Joy",
+	description: "Personal Website",
 };
 
 const atkinson = Atkinson_Hyperlegible({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-atkinson",
+	weight: "400",
+	subsets: ["latin"],
+	variable: "--font-atkinson",
 });
 
 export default function RootLayout({
-  children,
+	children,
 }: Readonly<{
-  children: React.ReactNode;
+	children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={`${atkinson.className}`}>{children}</body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body className={`${atkinson.className}`}>
+				{children}
+				<SpeedInsights />
+			</body>
+		</html>
+	);
 }
