88148a11bba9cb688613af46672dc289baa67799
RaySlash <stevemathewjoy@gmail.com>
2024-01-30 15:14:01 +1000

    css, navbar: update

--- src/app/Navbar.tsx
+++ src/app/Navbar.tsx
@@ -1,33 +1,37 @@
 import { FiGithub, FiMail } from "react-icons/fi";
+import Link from "next/link";
 
 function Navbar() {
-  return (
-    <div className="navitems">
-      <LeftNavContainer />
-      <RightNavContainer />
-    </div>
-  );
+	return (
+		<div className="navitems">
+			<LeftNavContainer />
+			<RightNavContainer />
+		</div>
+	);
 }
 
 function LeftNavContainer() {
-  return (
-    <div className="nav-left">
-      <p>Home</p> <p>Projects</p> <p>Photography</p> <p>Blog</p>
-    </div>
-  );
+	return (
+		<div className="nav-left">
+			<Link href="/">Home</Link>
+			<Link href="/projects">Projects</Link>
+			<Link href="/photography">Photography</Link>
+			<Link href="/blog">Blog</Link>
+		</div>
+	);
 }
 
 function RightNavContainer() {
-  return (
-    <div className="nav-right">
-      <a href="https://github.com/rayslash">
-        <FiGithub />
-      </a>
-      <a href="mailto:stevemathewjoy@tutanota.com">
-        <FiMail />
-      </a>
-    </div>
-  );
+	return (
+		<div className="nav-right">
+			<a href="https://github.com/rayslash">
+				<FiGithub />
+			</a>
+			<a href="mailto:stevemathewjoy@tutanota.com">
+				<FiMail />
+			</a>
+		</div>
+	);
 }
 
 export default Navbar;
